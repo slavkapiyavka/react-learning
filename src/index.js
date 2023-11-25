725b8d608a8fe43b5f1ffe061e6410223a70e4bf
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2023-11-26 00:06:25 +0100

    feat: add one question, create components folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import QuestionsList from './QuestionsList'
+import QuestionsList from './components/QuestionsList'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
