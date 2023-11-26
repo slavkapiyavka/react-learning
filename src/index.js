af3eb3d432e34eebc2e43ec35ac4adb3657a7bd2
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2023-11-26 23:07:42 +0100

    fix: rename QuestionsList component to List

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import QuestionsList from './components/QuestionsList';
+import List from './components/List';
 import Counter from './components/Counter';
+
+const QUESTIONS_LIST_TITLE = 'Questions';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <QuestionsList />
-    <Counter />
+    <List listTitle={QUESTIONS_LIST_TITLE} />
     <Counter />
   </React.StrictMode>
 );
