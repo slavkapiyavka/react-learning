94e717e648fb64e928cfc904e7ee2d9265b595f5
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2023-11-29 22:02:52 +0100

    feat: implement ChildComponent

--- src/components/state-chapter/ParentComponent.jsx
+++ src/components/state-chapter/ParentComponent.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import getRandomInteger from '../../utils/functions';
+import ChildComponent from './ChildComponent';
 
 const ParentComponent = () => {
   const [counter, setCounter] = useState(0);
@@ -16,14 +17,18 @@ const ParentComponent = () => {
   };
 
   return (
-    <aside className='card'>
-      <h2>Counter component</h2>
-      <p>{counter}</p>
-      <button onClick={incrementCounter} type="button">Увеличить</button>
-      <button onClick={resetCounter} type="button">Сбросить</button>
-      <button onClick={getRandomNumber} type="button">Случайное значение</button>
-      <button onClick={decrementCounter} type="button">Уменьшить</button>
-    </aside>
+    <>
+      <aside className='card'>
+        <h2>Counter component</h2>
+        <p>{counter}</p>
+        <button onClick={incrementCounter} type="button">Увеличить</button>
+        <button onClick={resetCounter} type="button">Сбросить</button>
+        <button onClick={getRandomNumber} type="button">Случайное значение</button>
+        <button onClick={decrementCounter} type="button">Уменьшить</button>
+      </aside>
+      <hr />
+      <ChildComponent count={counter}/>
+    </>
   );
 }
 
