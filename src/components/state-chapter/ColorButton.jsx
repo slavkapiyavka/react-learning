e0cc25bb5fe92ec4caf6e1b20faedc41670cc320
Vyacheslav <46050023+slavkapiyavka@users.noreply.github.com>
2023-11-30 02:07:29 +0100

    improve: put getRandomColor function into utils functions file

--- src/components/state-chapter/ColorButton.jsx
+++ src/components/state-chapter/ColorButton.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
+import getRandomColor from '../../utils/functions'
 
 const ColorButton = () => {
   const [color, setColor] = useState('#000');
-  const getRandomColor = () => `#${Math.floor(Math.random()*16777215).toString(16)}`;
   const textStyle = {
     color,
     fontSize: '24px',
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -3,4 +3,6 @@ const getRandomInteger = (min = 1, max = 10) => {
   return Math.floor(rand);
 }
 
-export default getRandomInteger;
+const getRandomColor = () => `#${Math.floor(Math.random()*16777215).toString(16)}`;
+
+export default {getRandomInteger, getRandomColor};
