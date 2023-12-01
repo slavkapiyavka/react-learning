f9f36e03c3c579329017844f384ea979a0407237
Vyacheslav <46050023+slavkapiyavka@users.noreply.github.com>
2023-12-01 16:56:39 +0100

    fix: update variable type in getRandomInteger function

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,5 +1,5 @@
 const getRandomInteger = (min = 1, max = 10) => {
-  let rand = min + Math.random() * (max + 1 - min);
+  const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
 
