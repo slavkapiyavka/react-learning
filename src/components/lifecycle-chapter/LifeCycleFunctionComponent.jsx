654a428783c86b87d34e5dfdb7a0c72603841e30
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2024-02-29 01:23:34 +0100

    fix: eslint warning about hooks dependency, display data

--- src/components/lifecycle-chapter/LifeCycleFunctionComponent.jsx
+++ src/components/lifecycle-chapter/LifeCycleFunctionComponent.jsx
@@ -1,29 +1,40 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-const fetchURL = 'https://jsonplaceholder.typicode.com/todos/1'
+const fetchURL = 'https://jsonplaceholder.typicode.com/todos'
 
 const LifeCycleFunctionComponent = () => {
   const [count, setCount] = useState(0)
+  const [data, setData] = useState({})
 
-  const buttonClickHandle = () => setCount((prevState) => prevState + 1)
+  const buttonClickHandle = () => {
+    setCount((prevState) => prevState + 1)
+  }
 
   useEffect(() => {
-    console.log('FC: component is mounted')
-
-    axios.get(fetchURL)
-      .then((response) => console.log('FC get data: ', response.data))
-      .catch((error) => console.error('FC get data error', error.message))
+    console.log('function component did mount')
 
     return () => {
-      console.log('FC: component will be unmounted')
+      console.log('function component will unmount')
     }
   }, [])
 
   useEffect(() => {
-    if (count % 2 === 0) {
-      console.log('FC: count value is changed: ', count)
+    const fetchData = async () => {
+      try {
+        const request = await axios.get(`${fetchURL}/${count}`)
+        setData({ ...request.data })
+
+      } catch (error) {
+        console.error('FC get data error: ', error.message)
+      }
     }
+
+    fetchData()
+  }, [count])
+
+  useEffect(() => {
+    console.log('function component did update with count value: ', count)
   }, [count])
 
   return (
@@ -31,6 +42,9 @@ const LifeCycleFunctionComponent = () => {
       <p>function component example</p>
       <p>{count}</p>
       <button onClick={buttonClickHandle}>increment counter</button>
+
+      <p>todo id: {data.id}</p>
+      <p>todo title: {data.title}</p>
     </div>
   );
 }
