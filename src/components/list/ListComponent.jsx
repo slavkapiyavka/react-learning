11bb20b2c17834a442b8aed8eccef5518d6694f8
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2024-03-05 00:03:03 +0100

    feat: implement ListComponent

--- src/components/list/ListComponent.jsx
+++ src/components/list/ListComponent.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const ListComponent = () => {
+const ListComponent = ({ data }) => {
+  const [list, setList] = useState([...data])
+  const handleButtonClick = (index) => {
+    setList((prev) => {
+      const upd = [...prev]
+      upd[index] = `🤖 ${upd[index]} 🤖`
+      return upd
+    })
+  }
+
+  const superHeroesList = list.map((element, index) => {
+    return (
+      <li key={index}>
+        <p>id: {index}</p>
+        <p>name: {element}</p>
+        <button
+          type="button"
+          onClick={() => handleButtonClick(index)}
+        >
+          {element.includes('🤖') ? 'more robo emojies!' : 'wrap name in robo emojies'}
+        </button>
+      </li>
+    )
+  })
+
+  useEffect(() => {
+    setList([...data])
+  }, [data])
+
   return (
     <ul>
-      <li>hello! i'm list component</li>
+      {superHeroesList}
     </ul>
   );
 }
